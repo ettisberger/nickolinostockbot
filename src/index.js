12d49bd77216a41fd4a557c20dce3281d1432358
Andy Ettisberger <andy@ettisberger.ch>
2021-01-22 23:27:39 +0100

    Switch api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
+const { IEXCloudClient } = require("node-iex-cloud");
 const BotToken = process.env.BOT_TOKEN;
 
 console.log(process.env.BOT_TOKEN)
 const bot = new Telegraf(BotToken);
 const axios = require("axios");
-const params = {
-  access_key: process.env.MARKETSTACK_API_KEY
-}
+// const params = {
+//   access_key: process.env.STOCK_REST_API_KEY
+// }
+
+const iex = new IEXCloudClient(axios, {
+  sandbox: false,
+  publishable: process.env.STOCK_REST_API_KEY,
+  version: "stable"
+})
+
 
 bot.start(ctx => {
   console.log(ctx.message.from)
@@ -23,7 +31,7 @@ bot.on('message', (ctx) => {
   console.log(ctx.message);
   if (ctx.message !== null && ctx.message.text != null && ctx.message.text.startsWith("$")) {
     const commands = ctx.message.text.split(" ");
-    const symbol =commands[0].substring(1);
+    const symbol = commands[0].substring(1);
 
     if (commands.length > 1) {
       handleCommands(commands, ctx, symbol);
@@ -42,14 +50,8 @@ function handleCommands(commands, ctx, symbol) {
 
   if (command) {
     switch (command) {
-      case "eod":
-        printEod(ctx, symbol);
-        break;
-      case "info":
-        printSymbolInfo(ctx, symbol);
-        break;
-      case "now":
-        printLatest(ctx, symbol);
+      case "crypto":
+        printCrypto(ctx, symbol);
         break;
       default:
         break;
@@ -57,45 +59,66 @@ function handleCommands(commands, ctx, symbol) {
   }
 }
 
-function printSymbolInfo(ctx, symbol) {
-  axios.get(process.env.MARKETSTACK_API_URL + "tickers/" + symbol, { params })
-  .then(response => {
-    console.log(response.data)
-
-    const data = response.data;
-    ctx.reply(`$${data.symbol}\nname => ${data.name}
-    \nstock exchange => ${data["stock_exchange"].name}
-    \nwebsite => ${data["stock_exchange"].website}
-    \ncity => ${data["stock_exchange"].city}
-    `);
-  }).catch(error => {
-    console.log(error.response.data);
-    ctx.reply(`Damn something went wrong >__>\n${error.response.data.error.message}`);
-  })
-}
+// function printSymbolInfo(ctx, symbol) {
+//   axios.get(process.env.MARKETSTACK_API_URL + "tickers/" + symbol, { params })
+//   .then(response => {
+//     console.log(response.data)
+//
+//     const data = response.data;
+//     ctx.reply(`$${data.symbol}\nname => ${data.name}
+//     \nstock exchange => ${data["stock_exchange"].name}
+//     \nwebsite => ${data["stock_exchange"].website}
+//     \ncity => ${data["stock_exchange"].city}
+//     `);
+//   }).catch(error => {
+//     console.log(error.response.data);
+//     ctx.reply(`Damn something went wrong >__>\n${error.response.data.error.message}`);
+//   })
+// }
 
 function printLatest(ctx, symbol) {
-  axios.get(process.env.MARKETSTACK_API_URL + "tickers/" + symbol + "/intraday/latest", { params })
-  .then(response => {
-    console.log(response.data)
+  iex.symbol(symbol).quote().then(data => {
+    console.log(data)
 
-    const data = response.data;
-    ctx.reply(`$${data['symbol']} last available stock price is $${data['last']} on ${data['date']}`);
+    ctx.reply(`$${data.symbol} (${data.companyName}, ${data.primaryExchange}) last available stock price is $${data.latestPrice}. Last update ${new Date(
+      data.latestUpdate)}`);
   }).catch(error => {
-    console.log(error.response.data);
-    ctx.reply(`Damn something went wrong >__>\n${error.response.data.error.message}`);
+    console.log(error);
+    ctx.reply(`Damn something went wrong >__>`);
+  });
+  // axios.get(process.env.MARKETSTACK_API_URL + "tickers/" + symbol + "/intraday/latest", { params })
+  // .then(response => {
+  //   console.log(response.data)
+  //
+  //   const data = response.data;
+  //   ctx.reply(`$${data['symbol']} last available stock price is $${data['last']} on ${data['date']}`);
+  // }).catch(error => {
+  //   console.log(error.response.data);
+  //   ctx.reply(`Damn something went wrong >__>\n${error.response.data.error.message}`);
+  // })
+}
+
+function printCrypto(ctx, symbol) {
+  iex.crypto(symbol).quote().then(data => {
+    console.log(data)
+
+    ctx.reply(`$${data.symbol} last available crypto price is $${data.latestPrice}. Last update ${new Date(
+      data.latestUpdate)}`);
+  }).catch(error => {
+    console.log(error);
+    ctx.reply(`Damn something went wrong >__>`);
   })
 }
 
-function printEod(ctx, symbol) {
-  axios.get(process.env.MARKETSTACK_API_URL + "tickers/" + symbol + "/eod/latest", { params })
-  .then(response => {
-    console.log(response.data)
-
-    const data = response.data;
-    ctx.reply(`$${data['symbol']} has a day high of $${data['high']} on ${data['date']}`);
-  }).catch(error => {
-    console.log(error.response.data);
-    ctx.reply(`Damn something went wrong >__>\n${error.response.data.error.message}`);
-  })
-}
+// function printEod(ctx, symbol) {
+//   axios.get(process.env.MARKETSTACK_API_URL + "tickers/" + symbol + "/eod/latest", { params })
+//   .then(response => {
+//     console.log(response.data)
+//
+//     const data = response.data;
+//     ctx.reply(`$${data['symbol']} has a day high of $${data['high']} on ${data['date']}`);
+//   }).catch(error => {
+//     console.log(error.response.data);
+//     ctx.reply(`Damn something went wrong >__>\n${error.response.data.error.message}`);
+//   })
+// }
