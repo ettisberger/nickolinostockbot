6ceb747e48adba85f7ff5e323dfdfb1e37894846
Andy Ettisberger <andy@ettisberger.ch>
2021-01-23 12:30:19 +0100

    Add example .env file

--- src/functions.js
+++ src/functions.js
@@ -8,54 +8,58 @@ const iex = new IEXCloudClient(axios, {
 })
 
 module.exports = {
-  info: function(ctx, symbol) {
+  info: function (ctx, symbol) {
     iex.symbol(symbol).company().then(data => {
       console.log(data)
-      ctx.reply(`$${symbol}\n
-      Company: ${data.companyName}\n
-      Exchange: ${data.exchange}\n
-      Industry: ${data.industry}\n
-      Website: ${data.website}\n
-      country: ${data.country}\n
-      city: ${data.city}\n
-      CEO: ${data.CEO}\n
+      ctx.reply(`$${symbol}\n\
+      Company: ${data.companyName}\n\
+      Exchange: ${data.exchange}\n\
+      Industry: ${data.industry}\n\
+      Website: ${data.website}\n\
+      country: ${data.country}\n\
+      city: ${data.city}\n\
+      CEO: ${data.CEO}\n\
       `)
     });
   },
-  latest: function(ctx, symbol) {
+  latest: function (ctx, symbol) {
     iex.symbol(symbol).quote().then(response => {
       console.log(response)
-      console.log(response.status)
 
-      ctx.reply(`$${response.symbol} (${response.companyName}, ${response.primaryExchange}) last available stock price is $${response.latestPrice}. Last update ${new Date(
-        response.latestUpdate)}`);
+      ctx.replyWithMarkdown(`$${response.symbol} (${response.companyName}, ${response.primaryExchange})\
+      \nLast available stock price is *$${response.latestPrice}*. \
+      \nDaily change: *${(response.changePercent * 100).toFixed(2)}%* ($${response.change})\
+      \n\nLast update ${new Date(response.latestUpdate)}`);
     }).catch(error => {
       console.log(error);
       ctx.reply(`Damn something went wrong >__>`);
     });
   },
-  cryptos: function(ctx, symbol) {
+  cryptos: function (ctx, symbol) {
     iex.crypto(symbol).quote().then(data => {
       console.log(data)
 
-      ctx.reply(`$${data.symbol} last available crypto price is $${data.latestPrice}. Last update ${new Date(
-        data.latestUpdate)}`);
+      ctx.replyWithMarkdown(`$${data.symbol} last available crypto price is *${data.latestPrice}*.\
+      \nDaily H/L: ${data.high ? data.high : "missing"}/${data.low ? data.low : "missing"}.\
+      \nPrevious close: ${data.previousClose ? data.previousClose : "missing"}\
+      \n\nLast update ${new Date(data.latestUpdate)}.`);
+
     }).catch(error => {
       console.log(error);
       ctx.reply(`Damn something went wrong >__>`);
     })
   },
-  dividends: function(ctx, symbol){
-  iex.symbol(symbol).dividends('dynamic').then(response => {
-    if(response.length === 0){
-      ctx.reply(`No dividends found for symbol $${symbol}.`);
-    } else {
-      const data = response[0];
-      ctx.reply(`$${data.symbol} pays out ${data.amount} ${data.currency}. Last payment on ${data.paymentDate}`);
-    }
-  }).catch(error => {
-    console.log(error);
-    ctx.reply(`Damn something went wrong >__>`);
-  })
-}
+  dividends: function (ctx, symbol) {
+    iex.symbol(symbol).dividends('dynamic').then(response => {
+      if (response.length === 0) {
+        ctx.reply(`No dividends found for symbol $${symbol}.`);
+      } else {
+        const data = response[0];
+        ctx.replyWithMarkdown(`$${data.symbol} pays out *${data.amount} ${data.currency}*. Last payment on ${data.paymentDate}`);
+      }
+    }).catch(error => {
+      console.log(error);
+      ctx.reply(`Damn something went wrong >__>`);
+    })
+  }
 };
