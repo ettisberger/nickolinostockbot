8254d391e3fa14712a5217137618d81075c53d0f
Andy Ettisberger <andy@ettisberger.ch>
2021-01-27 18:09:18 +0100

    Add trading quotes

--- src/messages.js
+++ src/messages.js
@@ -3,7 +3,19 @@ const quotes = [
   `\n\u{1F680} \u{1F680} \u{1F680} \u{1F680} \u{1F680}\n`,
   `\n\u{1F680} Sir, this is a casino. \u{1F680}\n`,
   `\n"Hello mom? Is my old room still available?"\n`,
-  `\nIf you don't find a way to make money while you sleep, you will work until you die.\n`
+  `\nIf you don't find a way to make money while you sleep, you will work until you die.\n`,
+  `\n\u{1F680} \u{1F680} \u{1F680} \u{1F680} \u{1F680}\n`,
+  `\nWhen it's raining gold, reach for a bucket, not a thimble.\n`,
+  `\nHope is bogus emotion that only costs you money.\n`,
+  `\n\u{1F680} Sir, this is a casino. \u{1F680}\n`,
+  `\nThe market is a device for transferring money from the impatient to the patient.\n`,
+  `\nThe market can stay irrational longer than you can stay solvent.\n`,
+  `\n\u{1F680} \u{1F680} \u{1F680} \u{1F680} \u{1F680}\n`,
+  `\nThe question should not be how much I will profit on this trade! The true question is; will I be fine if I don't profit from this trade.\n`,
+  `\n\u{1F680} Sir, this is a casino. \u{1F680}\n`,
+  `\nWe simply attempt to be fearful when others are greedy and to be greedy only when others are fearful.\n`,
+  `\nThe elements of good trading are (1) cutting losses, (2) cutting losses, and (3) cutting losses. If you can follow these three rules, you may have a chance.\n`,
+  `\nOne of the funny things about the stock market is that every time one person buys, another sells, and both think they are astute.\n`,
 ];
 
 module.exports = {
